94acdda1fae5116b256694a8b7c67416ab7c879b
KonaYukii <amyli07cd@gmail.com>
2022-03-20 17:34:26 -0400

    Added Option Input

--- app.py
+++ app.py
@@ -26,7 +26,20 @@ newFile = importFile.read().splitlines()
 for index, word in enumerate(newFile):
   textList.append(word)
 
-result = removeDuplicates(textList)
+# what you want to do with the file
+
+while True:
+  operationInput = input("What do you want to do with the file? Below are the options. Please input a number.\n 1. Remove duplicates\n 2. Do nothing\n")
+  if operationInput == "1":
+    result = removeDuplicates(textList) 
+    break
+  elif operationInput == "2":
+    print("Nothing happened")
+    importFile.close()
+    sys.exit(1)
+  else:
+    print("Please review the options and try again")
+
 # save the result into another text file
 userSave = input("What name do you want to save your file as?\n")
 fixedSavedFile = addedTxt(userSave)
