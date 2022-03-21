046c1589e68a65f1eae7c04b1a7dd7f7f87aed3b
KonaYukii <amyli07cd@gmail.com>
2022-03-21 17:05:56 -0400

    Added Unique Letters

--- app.py
+++ app.py
@@ -7,11 +7,22 @@ def removeDuplicates(sampleList):
   return returnList
 
 def addedTxt(userInput):
+  #adding txt to user input when saving file
   if userInput == '':
     userInput = 'result'
   fixedInput = userInput.split(".")
   finalFix = fixedInput[0] + ".txt"
   return finalFix
+
+def uniqueLetters(wordList):
+  # removing words that do not have unique letters
+  listOfWords = []
+  for word in wordList:
+    lowerWord = word.lower()
+    setWord = set(lowerWord)
+    if len(setWord) == len(word):
+      listOfWords.append(word)
+  return listOfWords
 
 # obtain information from the textfile
 textList = []
@@ -22,23 +33,28 @@ except:
   print("Import failed. Please try again.")
   sys.exit(1)
 newFile = importFile.read().splitlines()
-# loop through the words 
 for index, word in enumerate(newFile):
   textList.append(word)
 
 # what you want to do with the file
-
 while True:
-  operationInput = input("What do you want to do with the file? Below are the options. Please input a number.\n 1. Remove duplicates\n 2. Do nothing\n")
-  if operationInput == "1":
-    result = removeDuplicates(textList) 
-    break
-  elif operationInput == "2":
-    print("Nothing happened")
-    importFile.close()
+  operationInput = input("What do you want to do with the file? Below are the options. Please input a number.\n 1. Remove duplicates\n 2. Words with unique letters\n 3. Do nothing\n")
+  try:
+    if operationInput == "1":
+      result = removeDuplicates(textList) 
+      break
+    elif operationInput == "2":
+      result = uniqueLetters(textList)
+      break
+    elif operationInput == "3":
+      print("Nothing happened")
+      importFile.close()
+      sys.exit(1) 
+    else:
+      print("Please review the options and try again")
+  except:
+    print("System error. Please try again")
     sys.exit(1)
-  else:
-    print("Please review the options and try again")
 
 # save the result into another text file
 userSave = input("What name do you want to save your file as?\n")
